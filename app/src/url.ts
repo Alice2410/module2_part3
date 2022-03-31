b39726cef28d3bd66609503f203fa0a29968b5bb
Alice <devo4ka2410@gmail.com>
2022-03-31 18:35:12 +0300

    refactor: add /src to app folder
    add src directory to app and move all scripts to src folder
    
    MODULE2-PART3

---
+++ app/src/url.ts
@@ -0,0 +1,18 @@
+const loginURL = 'http://localhost:5000/authorization';
+const basicGalleryURL = 'http://localhost:5000/gallery';
+const localStorageTokenKey = 'token';
+const tokenTimestampKey = 'tokenReceiptTime';
+
+interface Token {
+    token: string;
+}
+
+interface Error {
+    errorMessage: string;
+}
+
+interface Gallery {
+    objects: string[];
+    page: number;
+    total: number;
+}
